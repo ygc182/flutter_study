5cd8a2524ed3f76a8c6d8aacc5f2cc030dbdb6f4
yuangc <yuanguangchao@xxbmm.com>
2019-05-17 15:33:10 +0800

    添加 StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -13,12 +13,29 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-        child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+//  @override
+//  State<StatefulWidget> createState() {
+//    return new RandomWordsState();
+//  }
 
+  // 简写
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
 
